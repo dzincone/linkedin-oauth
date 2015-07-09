706a2e9c802b4dea1d750a5699b8f0bf3e3877e2
Dylan Zincone <dylan.zincone@gmail.com>
2015-07-09 13:25:56 -0600

    Updated cookie-session to session

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cookie-session = require('cookie-session');
+var session = require('cookie-session');
 require('dotenv').load();
 
 var routes = require('./routes/index');
@@ -15,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('trust proxy', 1);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -23,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookie-session({ keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2] }))
+app.use(session({ keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2] }))
 
 app.use('/', routes);
 app.use('/users', users);
